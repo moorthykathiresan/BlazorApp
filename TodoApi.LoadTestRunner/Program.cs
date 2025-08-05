3b9e9405be39d828dfe0d6d1235a793b1f23bf81
moorthykathiresan <71226212+moorthykathiresan@users.noreply.github.com>
2025-08-05 03:07:27 +0000

    Added Load Test to the weatherforecast GET endpoint

---
+++ TodoApi.LoadTestRunner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var httpClient = new HttpClient();
+        var step = Step.Create("fetch_weather", async context =>
+        {
+            var response = await httpClient.GetAsync("http://localhost:5177/api/WeatherForecast");
+            return response.IsSuccessStatusCode
+                ? Response.Ok()
+                : Response.Fail();
+        });
+
+        var scenario = ScenarioBuilder.CreateScenario("weather_load_test", step)
+            .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+            .WithLoadSimulations(
+                Simulation.KeepConstant(copies: 20, during: TimeSpan.FromSeconds(30)),
+                Simulation.RampConstant(copies: 50, during: TimeSpan.FromSeconds(30))
+            );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+}
